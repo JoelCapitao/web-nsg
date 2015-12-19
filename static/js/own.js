20adbd1ac785c401f11f63083ba4b8374c63332c
Joel Capitao <joel.capitao93@gmail.com>
2015-12-19 11:43:01 +0100

    Percentage circle in project_display.html added

--- static/js/own.js
+++ static/js/own.js
@@ -37,3 +37,18 @@ $(function() {
         $('.modal:visible').each(reposition);
     });
 });
+
+
+/**
+ * Circle percent http://bootsnipp.com/snippets/Gq45X
+ */
+$(function(){
+      var $ppc = $('.progress-pie-chart'),
+        percent = parseInt($ppc.data('percent')),
+        deg = 360*percent/100;
+      if (percent > 50) {
+        $ppc.addClass('gt-50');
+      }
+      $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+      $('.ppc-percents span').html(percent+'%');
+    });
